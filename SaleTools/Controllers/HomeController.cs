fe078d6cde5cb1bf21b3c7b0843f99d4603179de
汪琪诗 <wangqishi_w@163.com>
2018-03-07 23:32:54 +0800

    首页加载商品

--- SaleTools/Controllers/HomeController.cs
+++ SaleTools/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,15 @@ namespace SaleTools.Controllers
 {
     public class HomeController : BaseController
     {
+
+        private GoodsManager _manager = new GoodsManager();
         // GET: Home
         public ActionResult Index()
         {
+            var user = (UserInfo)ViewBag.User;
+            //TODO   传的Id要根据用户的类型（管理员传自己的用户Id 普通用户传创建人Id）
+            var list = _manager.GetGoddsTypeTree(user.UserId);
+            ViewBag.GoddsTypeTree = list;
             return View();
         }
     }
